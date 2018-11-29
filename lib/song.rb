c9940243680c0e9e77b6159d44983cab83b58c11
kcluniesa <kcluniesa@yahoo.com>
2018-11-29 03:18:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,36 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    s = self.new
+    s.save
+    s
+  end
+
+  def self.new_by_name(string)
+    s = self.new 
+    s.name = string
+    s
+  end
+  
+  def self.create_by_name(string)
+    s = self.new
+    s.name = string
+    s.save
+    s 
+  end
+  
+  def self.find_by_name(string)
+    self.all.detect { |i| i.name == string}
+  end
+
+  def self.find_or_create_by_name(string)
+    c = self.find_by_name(string)
+    if c == nil 
+      self.create_by_name(string)
+    else
+      c
+    end
+  end 
+  
 end
